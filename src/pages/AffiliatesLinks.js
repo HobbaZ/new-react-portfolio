610254cacb12b9b18ddceca6dd22e43b728224d2
HobbaZ <zachobba@gmail.com>
2025-06-03 19:37:43 +1000

    Added to affiliate page

--- src/pages/AffiliatesLinks.js
+++ src/pages/AffiliatesLinks.js
@@ -7,7 +7,7 @@ import lego5 from "../images/lego5.jpg";
 import lego2 from "../images/lego2.JPG";
 import lego3 from "../images/lego3.JPG";
 import filament1 from "../images/amazon.JPG";
-import foam from "../images/foam.JPG";
+import pokemon from "../images/pokemon.JPG";
 import cat from "../images/cat.JPG";
 
 function Affiliates() {
@@ -23,6 +23,52 @@ function Affiliates() {
         <h1 style={{ color: `${userInputs.h1Color}` }}>Affiliate Links</h1>
 
         <div className="d-flex flex-wrap justify-content-left">
+          <Card className="col-sm-12 col-md-6 col-lg-4 p-2 border-0">
+            <Card.Header>
+              <Card.Title style={{ color: `${userInputs.h1Color}` }}>
+                Pokemon TCG Scarlet & Violet—151 6 Booster Pack Set
+              </Card.Title>
+              <img
+                className="card-img-top projectImage"
+                src={pokemon}
+                alt="Pokemon TCG Scarlet & Violet—151 Booster Bundle Set"
+              />
+            </Card.Header>
+
+            <Card.Body>
+              <Card.Text style={{ color: `${userInputs.pColor}` }}>
+                <p className="text-center" style={{ backgroundColor: "red" }}>
+                  15% off! Now $101.95
+                </p>{" "}
+                Discover the extraordinary Venusaur ex, Charizard ex, Mew ex &
+                Blastoise ex in special cards with captivating illustrations, as
+                well as the rest of the original 151 Pokemon from the Kanto
+                region. Set includes 6 booster packs.
+              </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+              <div className="text-center">
+                <a
+                  href="https://amzn.to/45CfEtO"
+                  rel="noreferrer"
+                  target="_blank"
+                  aria-label="Opens Amazon Fire TV Stick 4K streaming device product page in a new tab"
+                >
+                  <FormButton
+                    className="form-btn-primary"
+                    type="button"
+                    buttonGradientAngle={userInputs.buttonGradientAngle}
+                    buttonGradientColor1={userInputs.buttonGradientColor1}
+                    buttonGradientColor2={userInputs.buttonGradientColor2}
+                    buttonOutlineColor={userInputs.buttonOutlineColor}
+                    text={<div className="buttonText">Buy on Amazon</div>}
+                    colour={userInputs.buttonTextColor}
+                  ></FormButton>
+                </a>
+              </div>
+            </Card.Footer>
+          </Card>
+
           <Card className="col-sm-12 col-md-6 col-lg-4 p-2 border-0">
             <Card.Header>
               <Card.Title style={{ color: `${userInputs.h1Color}` }}>
